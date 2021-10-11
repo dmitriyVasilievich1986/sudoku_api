5751199bf39dec24f762948adad794562c86747a
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2021-10-11 10:40:21 +0300

    add AccountSerializer

---
+++ account/serializer.py
@@ -0,0 +1,8 @@
+from .models import Account
+from rest_framework.serializers import ModelSerializer
+
+
+class AccountSerializer(ModelSerializer):
+    class Meta:
+        model = Account
+        fields = "__all__"
