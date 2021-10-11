fcb314465a54df4247d13b767fd6caea2b3ae975
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2021-10-11 10:38:13 +0300

    add Account model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    username = models.CharField(
+        max_length=50,
+        unique=True,
+        blank=False,
+        null=False,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
