ae0bb501851f725fb1c27fc456d1b0daa1666104
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2021-10-13 10:43:10 +0300

    update account model add create method

--- account/models.py
+++ account/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from rest_framework import exceptions
 
 
 class Account(models.Model):
@@ -10,3 +11,13 @@ class Account(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    @classmethod
+    def create(cls: models.Model, username: str = None, *args: tuple, **kwargs: dict) -> models.Model:
+        if username is None or not isinstance(username, str):
+            raise exceptions.ValidationError(
+                {"username": "The field cannot be empty"})
+        if cls.objects.filter(username=username).count() > 0:
+            raise exceptions.ValidationError(
+                {"username": "This username is already in use"})
+        return cls(username=username)
