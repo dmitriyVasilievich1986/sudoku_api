d86d91e3a8062a172bc0115b610c317416e22ab9
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2021-10-18 21:31:04 +0300

    update serializer.py change save_with_id method

--- account/serializer.py
+++ account/serializer.py
@@ -1,8 +1,15 @@
 from .models import Account
 from rest_framework.serializers import ModelSerializer
+from rest_framework import exceptions
 
 
 class AccountSerializer(ModelSerializer):
     class Meta:
         model = Account
         fields = "__all__"
+
+    def save_with_id(self, id, *args, **kwargs):
+        instance: Account = Account.create(**self.validated_data)
+        instance.id = id
+        instance.save()
+        return instance
